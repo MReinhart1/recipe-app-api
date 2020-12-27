95e82ffeaf4231275c9df030992a3541a8541d39
Michael Reinhart <michaelreinhart112@gmail.com>
2020-12-26 21:10:29 -0500

    added custom user model

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ Directions:
 docker build .
 docker-compose build
 docker-compose run app sh -c "django-admin.py startproject app ."
+
+make travis.yml file and the flake8 file and push it to github
+
+add the repo to travis and then everytime you fail you get an email about it
+
+
+To do testing (use this all the time):
+docker-compose run app sh -c "python manage.py test && flake8" 
+
+to start the core to hold all the things that this app uses to run:
+docker-compose run app sh -c "python manage.py startapp core"
+
+
+This line is to make the migrations of data into database 
+Run this everytime you want to  change your models
+located because of ===  AUTH_USER_MODEL = 'core.User (in settings.py)
+docker-compose run app sh -c "python manage.py makemigrations core"
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_success(self):
+        email = 'test@gmail.com'
+        password="password"
+        user = get_user_model().objects.create_user(
+        email=email,
+        password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+    
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_superUser(self):
+        user = get_user_model().objects.create_superuser(
+            'michael@gmail.com',
+            "passowrd"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+
+
+
+    
